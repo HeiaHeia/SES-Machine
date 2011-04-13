694566a8e1a5f3c3a8db0c2dfc6a5ec010639ff4
Kirill Nikitin <locke23rus@gmail.com>
2011-04-13 12:50:08 +0400

    Add catch ResponseError

--- lib/ses_machine/mailer.rb
+++ lib/ses_machine/mailer.rb
@@ -6,22 +6,27 @@ module SesMachine #:nodoc:
     def perform_delivery_ses_machine(mail)
       mail = Mail.read_from_string(mail.encoded) if ActionMailer::VERSION::MAJOR < 3
       raw_source = SesMachine.use_dkim? ? sign_mail(mail) : mail.encoded
-      # TODO: rescue ResponseError
-      response = ActionMailer::Base.custom_amazon_ses_mailer.send_raw_email(raw_source, :source => SesMachine.email_account)
-  
+
+      begin
+        response = ActionMailer::Base.custom_amazon_ses_mailer.send_raw_email(raw_source, :source => SesMachine.email_account)
+      rescue AWS::SES::ResponseError => e
+        response = e.response
+      end
+
       doc = {
         :address => mail.to,
         :subject => mail.subject,
         :raw_source => raw_source,
         :date => response.headers['date'].to_time.utc,
-        :message_id => response.message_id,
         :request_id => response.request_id,
         :response => response.to_s,
         :response_code => response.code,
         :response_error => response.error.to_s,
-        :bounce_type => 0,
+        :bounce_type => response.error? ? SesMachine::Bounce::TYPES[:unknown] : 0,
         '_keywords' => SesMachine::DB.get_keywords(mail.to + mail.subject.split)
       }
+
+      doc.merge!(:message_id => response.message_id) unless response.error?
 
       SesMachine.database['mails'].insert(doc)
     end
