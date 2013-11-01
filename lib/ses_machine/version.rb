55414c5f3825cbcf7c9f05c7da57f2bfe81cda1e
Kirill Nikitin <locke23rus@gmail.com>
2013-11-01 23:12:31 +0400

    Released 0.0.1

--- lib/ses_machine/version.rb
+++ lib/ses_machine/version.rb
@@ -1,12 +1,11 @@
 # -*- encoding : utf-8 -*-
 
-
 module SesMachine #:nodoc
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 0
     TINY  = 1
-    PRE   = 'dev'
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
